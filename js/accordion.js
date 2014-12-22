358bb70659a44161d826056344fe38fe89abfb40
e.khurtina <e.khurtina@rambler-co.ru>
2014-12-22 18:58:48 +0300

    [+] arrows+js

--- js/accordion.js
+++ js/accordion.js
@@ -5,19 +5,19 @@
   $('.accordion > dt > a').click(function () {
     $(this).parent().toggleClass('open');
     allPanels.slideUp();
-    $('.arrow_bottom.filters__arrow_direction_top').removeClass('arrow_bottom filters__arrow_direction_top')
-      .addClass('arrow_up filters__arrow_direction_bottom');
+    $('.arrow.arrow_top').removeClass('arrow_top')
+      .addClass('arrow_bottom');
 
     if ($(this).parent().hasClass('open')) {
-      $(this).parent().find('.arrow_up.filters__arrow_direction_bottom')
-        .addClass('arrow_bottom filters__arrow_direction_top')
-        .removeClass('arrow_up filters__arrow_direction_bottom');
+      $(this).parent().find('.arrow.arrow_bottom')
+        .addClass('arrow_top')
+        .removeClass('arrow_bottom');
       $(this).parent().next().slideDown();
     }
     else {
-      $(this).parent().find('.arrow_bottom.filters__arrow_direction_top')
-        .removeClass('arrow_bottom filters__arrow_direction_top')
-        .addClass('arrow_up filters__arrow_direction_bottom');
+      $(this).parent().find('.arrow_top')
+        .removeClass('arrow_top')
+        .addClass('arrow_bottom');
       allPanels.slideUp();
     }
     return false;
